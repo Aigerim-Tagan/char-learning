96a1a9c101ef7d87c552784cde76672c80cc1508
Aigerim-Tagan <elf.kai13@gmail.com>
2024-02-25 03:44:51 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        char fstLetter = 'a', scndLetter = 'i', thrdLetter = 'g';
+        int fstLetterNum = (int) fstLetter;
+        int scndLetterNum = (int) scndLetter;
+        int thrdLetterNum = (int) thrdLetter;
+        int forthLetterNum = (int) 'e';
+        int fifthLetterNum = (int) 'r';
+        int sixthLetterNum = (int) 'm';
+
+        System.out.println(fstLetterNum + ", " + scndLetterNum + ", " + thrdLetterNum + ", "
+                + forthLetterNum + ", " + fifthLetterNum + ", " + scndLetterNum + ", " + sixthLetterNum);
+        char oLetter = 97, twLetter = 105;
+        char oLetterNew = (char) oLetter;
+        char twLetterNew = twLetter;
+        char tLetterNew = (char) thrdLetterNum;
+        char fLetterNew = 101;
+        char fifLetterNew = 114, sixLetterNew = 109;
+
+        System.out.print(oLetterNew + ", " + twLetterNew + ", " + tLetterNew + ", " + fLetterNew
+                + ", " + fifLetterNew + ", " + twLetterNew + ", " + sixLetterNew);
+    }
+}
